5040f01d4b55ffa37c63ffa9180eb0d1ee53401a
Tim Verheijen <70171119+Tim-Vrhn@users.noreply.github.com>
2024-06-12 14:08:19 +0200

    Correctly display GMT offset

--- selection.js
+++ selection.js
@@ -50,7 +50,7 @@ function timestampToDate(ts) {
 
 function getLocalString(date) {
   tz = date.getTimezoneOffset()
-  return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())} ${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())} GMT${tz < 0 ? '+' : '-'}${pad(Math.floor(tz / 60))}:${pad(tz % 60)}`
+  return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())} ${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())} GMT${tz < 0 ? '+' : '-'}${pad(Math.abs(Math.floor(tz) / 60))}:${pad(tz % 60)}`
 }
 
 function getUTCString(date) {
